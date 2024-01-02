93e0594f8f572af6ea8adedfc3777c8c478b71c7
Kamvitae Surimushado <kamvitae@gmail.com>
2024-01-02 21:58:35 +0100

    Add project files.

---
+++ Guy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HF_ch11_DataContracts
+{
+    [DataContract(Namespace = "HF_ch11_DataContracts")]
+    class Guy
+    {
+        [DataMember(Name="Name")]
+        public string Name { get; private set; }
+        [DataMember(Name = "Age")]
+        public int Age { get; private set; }
+        [DataMember(Name = "Cash")]
+        public decimal Cash { get; private set; }
+        [DataMember(Name = "MyCard")]
+        public Card TrumpCard { get; set; }
+        public override string ToString()=>
+            String.Format($"My name is {Name}, I'm {Age}, I have {Cash} bucks, and my trump card is {TrumpCard}.");
+        
+
+        public Guy(string name, int age, decimal cash)
+        {
+            Name = name;
+            Age = age;
+            Cash = cash;
+            TrumpCard = Card.RandomCard();
+        }
+    }
+}
---
+++ GuyManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace HF_ch11_DataContracts
+{
+    class GuyManager : INotifyPropertyChanged
+    {
+        private Guy joe = new Guy("Joe", 37, 176.22M);
+        public Guy Joe { get => joe; }
+
+        private Guy bob = new Guy("Bob", 45, 4.68M);
+        public Guy Bob { get => bob; }
+
+        private Guy ed = new Guy("Ed", 43, 377.51M);
+        public Guy Ed { get => ed; }
+
+        public Guy NewGuy { get; set; }
+        public string GuyFile { get; set; }
+        public void ReadGuy()
+        {
+            if (String.IsNullOrEmpty(GuyFile))
+                return;
+
+            using (Stream inputStream = File.OpenRead(GuyFile))
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(Guy));
+                NewGuy = serializer.ReadObject(inputStream) as Guy;
+            }
+            OnPropertyChanged("NewGuy");
+        }
+        public void WriteGuy(Guy guyToWrite)
+        {
+            GuyFile = Path.GetFullPath(guyToWrite.Name + ".xml");
+
+            if (File.Exists(GuyFile))
+                File.Delete(GuyFile);
+            using (Stream outpuStream = File.OpenWrite(GuyFile))
+            {
+                DataContractSerializer serializer = new DataContractSerializer(typeof(Guy));
+                serializer.WriteObject(outpuStream, guyToWrite);
+            }
+            OnPropertyChanged("GuyFile");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChangedEvent = PropertyChanged;
+            if (propertyChangedEvent != null)
+                propertyChangedEvent(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
